e77d766a200544439aefa265e0adb07e4e5c523c
ibarajas248 <ibarajas@unal.edu.co>
2023-10-15 23:58:35 -0500

    1

---
+++ app/src/main/java/com/example/mysql_androidstudio/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.mysql_androidstudio;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtCodigo, edtProducto, edtprecio, edtFabricante;
+    Button btnAgregar, btnBuscar, btnEditar, btnEliminar;
+
+    RequestQueue requestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtCodigo=(EditText) findViewById(R.id.edtCodigo);
+        edtProducto=(EditText) findViewById(R.id.edtproducto);
+        edtprecio=(EditText) findViewById(R.id.edtprecio);
+        edtFabricante=(EditText) findViewById(R.id.edtfabricante);
+        btnAgregar=(Button) findViewById(R.id.btnagregar);
+        btnBuscar=(Button) findViewById(R.id.btnbuscar);
+        btnEditar=(Button) findViewById(R.id.btneditar);
+        btnEliminar=(Button) findViewById(R.id.btneliminar);
+
+
+
+
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ejecutarServicio("http://192.168.0.12/developeru/insertar_producto.php");
+                ejecutarServicio("http://khushiconfecciones.com//app_android/insertar_producto.php");
+                //ejecutarServicio("https://154.49.247.154/app_android/insertar_producto.php");
+               // https://srv1069-files.hstgr.io/89d93855ab3fea79/files/public_html/app_android/conexion_1.php
+                //ejecutarServicio("https://srv1069-files.hstgr.io/89d93855ab3fea79/files/public_html/app_android/conexion_1.php");
+
+
+
+            }
+        });
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buscarProducto("http://192.168.0.12/developeru/buscar_producto.php?codigo=" + edtCodigo.getText().toString());
+
+            }
+        });
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutarServicio("http://192.168.0.12/developeru/editar_producto.php");
+
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                eliminarProducto("http://192.168.0.12/developeru/eliminar_producto.php");
+            }
+        });
+
+    }
+
+
+
+    private void ejecutarServicio (String URL){
+        // Crear una solicitud de cadena (StringRequest) con un método POST
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Este método se llama cuando la solicitud es exitosa
+                // response contiene la respuesta del servidor en formato de cadena
+
+                Toast.makeText(MainActivity.this, "Operacion Exitosa", Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Este método se llama si hay un error en la solicitud
+                // error contiene detalles del error, como un mensaje de error
+
+                Toast.makeText(MainActivity.this, error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                // Este método se utiliza para definir los parámetros que se enviarán en la solicitud POST
+                // Debes especificar los parámetros que el servidor espera, como "codigo", "producto", "precio", "fabricante"
+
+
+
+                Map<String, String> parametros= new HashMap<String, String>();
+                parametros.put("codigo",edtCodigo.getText().toString());
+                parametros.put("producto",edtProducto.getText().toString());
+                parametros.put("precio",edtprecio.getText().toString());
+                parametros.put("fabricante",edtFabricante.getText().toString());
+
+                return parametros;
+            }
+        };
+
+        // Agregar la solicitud a la cola de solicitudes de Volley para que se envíe al servidor
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void buscarProducto (String URL){
+        // Crear una solicitud JSON (JsonArrayRequest) con un método GET
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                // Este método se llama cuando la solicitud es exitosa y recibe un JSONArray como respuesta
+                JSONObject jsonObject=null;
+                for(int i=0;i<response.length();i++){
+                    try{
+                        jsonObject=response.getJSONObject(i);
+                        // Extraer datos del objeto JSON y mostrarlos en los campos de texto
+
+                        edtProducto.setText(jsonObject.getString("producto"));
+                        edtprecio.setText(jsonObject.getString("precio"));
+                        edtFabricante.setText(jsonObject.getString("fabricante"));
+
+                    }catch (JSONException e){
+                        // Capturar y mostrar cualquier error JSON que ocurra
+                        Toast.makeText(getApplicationContext(),e.getMessage() , Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de conexion", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, error.toString(),Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
+        );
+        requestQueue=Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
+    private void eliminarProducto (String URL){
+        // Crear una solicitud de cadena (StringRequest) con un método POST
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Este método se llama cuando la solicitud es exitosa
+                // response contiene la respuesta del servidor en formato de cadena
+
+                Toast.makeText(MainActivity.this, "El producto FUE Eliminado con exito", Toast.LENGTH_SHORT).show();
+                limpiarFormulario();
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Este método se llama si hay un error en la solicitud
+                // error contiene detalles del error, como un mensaje de error
+
+                Toast.makeText(MainActivity.this, error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                // Este método se utiliza para definir los parámetros que se enviarán en la solicitud POST
+                // Debes especificar los parámetros que el servidor espera, como "codigo", "producto", "precio", "fabricante"
+
+
+
+                Map<String, String> parametros= new HashMap<String, String>();
+                parametros.put("codigo",edtCodigo.getText().toString());
+
+
+                return parametros;
+            }
+        };
+
+        // Agregar la solicitud a la cola de solicitudes de Volley para que se envíe al servidor
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void limpiarFormulario(){
+        edtCodigo.setText("");
+        edtProducto.setText("");
+        edtprecio.setText("");
+        edtFabricante.setText("");
+
+
+    }
+}
